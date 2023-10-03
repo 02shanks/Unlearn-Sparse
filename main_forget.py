9ee2f2f7bcbfbf21146ca0e9626c70f0d3053eea
SHIV-anna <shivshankar07123@gmail.com>
2023-10-03 20:51:01 +0530

    changed lora model

--- main_forget.py
+++ main_forget.py
@@ -182,32 +182,32 @@ def main():
         
         if args.lora=='YES':
             print("Adding LoRA training mode")
-            model_state_dict = model.state_dict()
+            # model_state_dict = model.state_dict()
 
-            for key in checkpoint['state_dict'].keys():
-                if "mask" in key or 'orig' in key:
-                    raw_key = key.split('_')[0]
-                    orig_w_key = raw_key + '_orig'
-                    mask_w_key = raw_key + '_mask'
+            # for key in checkpoint['state_dict'].keys():
+            #     if "mask" in key or 'orig' in key:
+            #         raw_key = key.split('_')[0]
+            #         orig_w_key = raw_key + '_orig'
+            #         mask_w_key = raw_key + '_mask'
 
-                    # Check if orig and mask keys exist in the checkpoint
-                    if orig_w_key not in checkpoint['state_dict'] or mask_w_key not in checkpoint['state_dict']:
-                        raise KeyError(f"Missing orig/mask keys for {raw_key}")
+            #         # Check if orig and mask keys exist in the checkpoint
+            #         if orig_w_key not in checkpoint['state_dict'] or mask_w_key not in checkpoint['state_dict']:
+            #             raise KeyError(f"Missing orig/mask keys for {raw_key}")
 
-                    # Extract original weight (A) and mask (B)
-                    A = checkpoint['state_dict'][orig_w_key]
-                    B = checkpoint['state_dict'][mask_w_key]
+            #         # Extract original weight (A) and mask (B)
+            #         A = checkpoint['state_dict'][orig_w_key]
+            #         B = checkpoint['state_dict'][mask_w_key]
                     
-                    # Check if A and B have compatible shapes
-                    if A.shape != B.shape:
-                        raise ValueError(f"Shapes of {orig_w_key} and {mask_w_key} do not match")
+            #         # Check if A and B have compatible shapes
+            #         if A.shape != B.shape:
+            #             raise ValueError(f"Shapes of {orig_w_key} and {mask_w_key} do not match")
 
-                    # Perform pointwise multiplication and assign to the original key in the model's state_dict
-                    model_state_dict[raw_key] = A.mul(B)
+            #         # Perform pointwise multiplication and assign to the original key in the model's state_dict
+            #         model_state_dict[raw_key] = A.mul(B)
 
-                else:
-                    # Assign the same weight in the checkpoint to the model
-                    model_state_dict[key] = checkpoint['state_dict'][key]
+            #     else:
+            #         # Assign the same weight in the checkpoint to the model
+            #         model_state_dict[key] = checkpoint['state_dict'][key]
                 
             target_modules=['model.conv1',
                         'model.layer1.0.conv1',
