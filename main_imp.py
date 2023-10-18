8a25696e9d6766b64f130d99c90a8b83ea4a3d57
SHIV-anna <shivshankar07123@gmail.com>
2023-10-18 18:57:42 +0530

    added hf_resent arg

--- main_imp.py
+++ main_imp.py
@@ -56,7 +56,7 @@ def main():
         model, train_loader, val_loader, test_loader, marked_loader = setup_model_dataset(
             args)
         
-    if args.hf_resnet=='True':
+    if args.hf_resnet=='YES':
         print("loading hf_resnet model")
         model = ResNetForImageClassification.from_pretrained("microsoft/resnet-50")
 
